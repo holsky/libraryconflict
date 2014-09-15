84b35430aaa629eac1cd76fe3b0387d1a13e3c9f
Holger Schmeisky <google@schmeisky.com>
2014-09-15 09:50:53 +0200

    setup up plugin module with library conflict

---
+++ src/mypackage/TestConflict.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+/**
+ * Created by holger on 15.09.14.
+ */
+public class TestConflict {
+
+    public void test() {
+
+    }
+}
---
+++ src/mypackage/TestConflictTest.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestConflictTest extends LightPlatformCodeInsightFixtureTestCase {
+
+    @Test
+    public void testTest() throws Exception {
+
+    }
+}
